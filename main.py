12f894d68b00934e5ec11ea789d3ae17f5bfd374
Galvão <145558719+ItsGalvo@users.noreply.github.com>
2024-07-03 17:45:15 -0300

    Update main.py

--- main.py
+++ main.py
@@ -31,5 +31,10 @@ def get_root(request: Request):
     view_model = {"request": request}
     return templates.TemplateResponse("teste.html", view_model)
 
+@app.get("/carrinho")
+def get_root(request: Request):
+    view_model = {"request": request}
+    return templates.TemplateResponse("teste.html", view_model)
+
 if __name__ == "__main__":
     uvicorn.run(app="main:app", port=8000, reload=True)
