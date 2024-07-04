96cd9d3d0b9404f394c16159b36febf49fba2865
Galvão <p.galvaolinhares@gmail.com>
2024-07-04 08:33:42 -0300

    Teste commit pelo VScode

--- routes/main_routes.py
+++ routes/main_routes.py
@@ -3,33 +3,35 @@ from fastapi.responses import HTMLResponse
 
 from util.templates import obter_jinja_templates
 
+
+
 router = APIRouter()
 templates = obter_jinja_templates("templates/main")
 
 @router.get("/")
 def get_root(request: Request):
     view_model = {"request": request}
-    return templates.TemplateResponse("index.html", view_model)
+    return templates.TemplateResponse("pages/index.html", view_model)
 
 @router.get("/contato")
 def get_root(request: Request):
     view_model = {"request": request}
-    return templates.TemplateResponse("contato.html", view_model)
+    return templates.TemplateResponse("pages/contato.html", view_model)
 
 @router.get("/store")
 def get_root(request: Request):
     view_model = {"request": request}
-    return templates.TemplateResponse("store.html", view_model)
+    return templates.TemplateResponse("pages/store.html", view_model)
 
 @router.get("/teste")
 def get_root(request: Request):
     view_model = {"request": request}
-    return templates.TemplateResponse("teste.html", view_model)
+    return templates.TemplateResponse("pages/teste.html", view_model)
 
 @router.get("/carrinho")
 def get_root(request: Request):
     view_model = {"request": request}
-    return templates.TemplateResponse("teste.html", view_model)
+    return templates.TemplateResponse("pages/carrinho.html", view_model)
 
 @router.get("/", response_class=HTMLResponse)
 async def get_root(request: Request):
