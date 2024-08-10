10cb2f2382941f2cee15f0b6870ba5e7ccced44c
Galvão <145558719+ItsGalvo@users.noreply.github.com>
2024-08-10 13:21:22 -0300

    Add files via upload

--- routes/main_routes.py
+++ routes/main_routes.py
@@ -8,30 +8,51 @@ from util.templates import obter_jinja_templates
 router = APIRouter()
 templates = obter_jinja_templates("templates/main")
 
-@router.get("/pages/")
+@router.get("/")
 def get_root(request: Request):
     view_model = {"request": request}
     return templates.TemplateResponse("pages/index.html", view_model)
 
-@router.get("/pages/contato")
+@router.get("/contato")
 def get_root(request: Request):
     view_model = {"request": request}
     return templates.TemplateResponse("pages/contato.html", view_model)
 
-@router.get("/pages/store")
+@router.get("/store")
 def get_root(request: Request):
     view_model = {"request": request}
     return templates.TemplateResponse("pages/store.html", view_model)
 
-@router.get("/pages/teste")
+@router.get("/login")
 def get_root(request: Request):
     view_model = {"request": request}
-    return templates.TemplateResponse("pages/teste.html", view_model)
+    return templates.TemplateResponse("pages/login.html", view_model)
 
-@router.get("/pages/carrinho")
+@router.get("/loginusuario1")
+def get_root(request: Request):
+    view_model = {"request": request}
+    return templates.TemplateResponse("pages/loginusuario1.html", view_model)
+
+@router.get("/loginusuario2")
+def get_root(request: Request):
+    view_model = {"request": request}
+    return templates.TemplateResponse("pages/loginusuario2.html", view_model)
+
+@router.get("/loginusuario3")
+def get_root(request: Request):
+    view_model = {"request": request}
+    return templates.TemplateResponse("pages/loginusuario3.html", view_model)
+
+@router.get("/carrinho")
 def get_root(request: Request):
     view_model = {"request": request}
     return templates.TemplateResponse("pages/carrinho.html", view_model)
+
+
+@router.get("/perfil")
+def get_root(request: Request):
+    view_model = {"request": request}
+    return templates.TemplateResponse("pages/perfil.html", view_model)
 
 @router.get("/", response_class=HTMLResponse)
 async def get_root(request: Request):
