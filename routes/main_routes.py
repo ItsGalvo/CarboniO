a37789afb5d6c500627fb81ccd43efee56ca5f56
Galvão <145558719+ItsGalvo@users.noreply.github.com>
2024-09-20 09:19:49 -0300

    Add files via upload

--- routes/main_routes.py
+++ routes/main_routes.py
@@ -13,6 +13,16 @@ def get_root(request: Request):
     view_model = {"request": request}
     return templates.TemplateResponse("pages/index.html", view_model)
 
+@router.get("/termosdeuso")
+def get_root(request: Request):
+    view_model = {"request": request}
+    return templates.TemplateResponse("pages/termosdeuso.html", view_model)
+
+@router.get("/politicaprivacidade")
+def get_root(request: Request):
+    view_model = {"request": request}
+    return templates.TemplateResponse("pages/politicaprivacidade.html", view_model)
+
 @router.get("/centrodecoleta")
 def get_root(request: Request):
     view_model = {"request": request}
